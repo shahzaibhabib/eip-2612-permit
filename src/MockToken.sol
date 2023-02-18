f57ef8b9ee7c5600284040f3f718af6d32efcf87
Shahzaib Habib <code.shahzaibhabib@gmail.com>
2023-02-18 15:42:48 +0500

    feat: add eip-712 permit functionality

--- src/MockToken.sol
+++ src/MockToken.sol
@@ -4,6 +4,20 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract MockToken is IERC20 {
+    // bytes32 private constant EIP712DOMAIN_HASH =
+    // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
+    bytes32 private constant EIP712DOMAIN_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
+
+    // bytes32 private constant NAME_HASH = keccak256("Mock Token")
+    bytes32 private constant NAME_HASH = 0xeb96b779e2a421c78f42d9b3999c8bc0c7726c66c63accdd9ba94f2ff1357844;
+
+    // bytes32 private constant VERSION_HASH = keccak256("1")
+    bytes32 private constant VERSION_HASH = 0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6;
+
+    // bytes32 public constant PERMIT_TYPEHASH =
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 private constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
     string public constant name = "Mock Token";
     string public constant symbol = "MOCK";
     uint8 public constant decimals = 18;
@@ -13,7 +27,16 @@ contract MockToken is IERC20 {
     mapping(address => uint256) public override balanceOf;
     mapping(address => mapping(address => uint256)) public allowances;
 
+    // EIP-2612
+    mapping(address => uint256) public nonces;
+
     constructor() {}
+
+    function _validateSignedData(address signer, bytes32 encodeData, uint8 v, bytes32 r, bytes32 s) internal view {
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", getDomainSeparator(), encodeData));
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == signer, "MockToken:: INVALID_SIGNATURE");
+    }
 
     function _mint(address to, uint256 value) internal {
         totalSupply = totalSupply + value;
@@ -37,6 +60,17 @@ contract MockToken is IERC20 {
         balanceOf[from] = balanceOf[from] - value;
         balanceOf[to] = balanceOf[to] + value;
         emit Transfer(from, to, value);
+    }
+
+    function getChainId() public view returns (uint256 chainId) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            chainId := chainid()
+        }
+    }
+
+    function getDomainSeparator() public view returns (bytes32) {
+        return keccak256(abi.encode(EIP712DOMAIN_HASH, NAME_HASH, VERSION_HASH, getChainId(), address(this)));
     }
 
     function mint(address to, uint256 value) external returns (bool) {
@@ -70,5 +104,17 @@ contract MockToken is IERC20 {
         }
         _transfer(from, to, value);
         return true;
+    }
+
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external
+    {
+        require(deadline >= block.timestamp, "MockToken:: AUTH_EXPIRED");
+
+        bytes32 encodeData = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner], deadline));
+        nonces[owner] = nonces[owner] + 1;
+        _validateSignedData(owner, encodeData, v, r, s);
+
+        _approve(owner, spender, value);
     }
 }
