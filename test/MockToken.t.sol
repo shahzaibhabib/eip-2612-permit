efcbb4f72534fc9a3174d2f96fc686a780525376
Shahzaib Habib <code.shahzaibhabib@gmail.com>
2023-02-18 17:29:29 +0500

    test: implement tests for depositing contract scenario

--- test/MockToken.t.sol
+++ test/MockToken.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "./../src/MockToken.sol";
 import "./../src/SigUtils.sol";
+import "./../src/Deposit.sol";
 
 contract ERC20Test is Test {
     MockToken internal token;
@@ -175,5 +176,77 @@ contract ERC20Test is Test {
 
         vm.prank(spender);
         token.transferFrom(owner, spender, 2e18); // attempt to transfer 2 tokens (owner only owns 1)
+    }
+}
+
+contract DepositTest is Test {
+    Deposit internal deposit;
+    MockToken internal token;
+    SigUtils internal sigUtils;
+
+    uint256 internal ownerPrivateKey;
+    address internal owner;
+
+    function setUp() public {
+        deposit = new Deposit();
+        token = new MockToken();
+        sigUtils = new SigUtils(token.getDomainSeparator());
+
+        ownerPrivateKey = 0xA11CE;
+        owner = vm.addr(ownerPrivateKey);
+
+        token.mint(owner, 1e18);
+    }
+
+    function test_DepositWithLimitedPermit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: address(deposit),
+            value: 1e18,
+            nonce: token.nonces(owner),
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        deposit.depositWithPermit(
+            address(token), 1e18, permit.owner, permit.spender, permit.value, permit.deadline, v, r, s
+        );
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(address(deposit)), 1e18);
+
+        assertEq(token.allowance(owner, address(deposit)), 0);
+        assertEq(token.nonces(owner), 1);
+
+        assertEq(deposit.userDeposits(owner, address(token)), 1e18);
+    }
+
+    function test_DepositWithMaxPermit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: address(deposit),
+            value: type(uint256).max,
+            nonce: token.nonces(owner),
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        deposit.depositWithPermit(
+            address(token), 1e18, permit.owner, permit.spender, permit.value, permit.deadline, v, r, s
+        );
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(address(deposit)), 1e18);
+
+        assertEq(token.allowance(owner, address(deposit)), type(uint256).max);
+        assertEq(token.nonces(owner), 1);
+
+        assertEq(deposit.userDeposits(owner, address(token)), 1e18);
     }
 }
