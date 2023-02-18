065cd5e72a34cb89e37b00c2e03e1ced2668cc2b
Shahzaib Habib <code.shahzaibhabib@gmail.com>
2023-02-18 14:45:06 +0500

    feat: implement ERC20 interface with mint & burn functionality

--- src/MockToken.sol
+++ src/MockToken.sol
@@ -1,14 +1,74 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// contract Counter {
-//     uint256 public number;
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-//     function setNumber(uint256 newNumber) public {
-//         number = newNumber;
-//     }
+contract MockToken is IERC20 {
+    string public constant name = "Mock Token";
+    string public constant symbol = "MOCK";
+    uint8 public constant decimals = 18;
 
-//     function increment() public {
-//         number++;
-//     }
-// }
+    uint256 public override totalSupply;
+
+    mapping(address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint256)) public allowances;
+
+    constructor() {}
+
+    function _mint(address to, uint256 value) internal {
+        totalSupply = totalSupply + value;
+        balanceOf[to] = balanceOf[to] + value;
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint256 value) internal {
+        balanceOf[from] = balanceOf[from] - value;
+        totalSupply = totalSupply = value;
+        emit Transfer(from, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint256 value) private {
+        allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    function _transfer(address from, address to, uint256 value) private {
+        require(to != address(this) && to != address(0), "MockToken :: RECEIVER_IS_TOKEN_OR_ZERO");
+        balanceOf[from] = balanceOf[from] - value;
+        balanceOf[to] = balanceOf[to] + value;
+        emit Transfer(from, to, value);
+    }
+
+    function mint(address to, uint256 value) external returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+
+    function burn(uint256 value) external returns (bool) {
+        _burn(msg.sender, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) external override returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return allowances[owner][spender];
+    }
+
+    function transfer(address to, uint256 value) external override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external override returns (bool) {
+        uint256 fromAllowance = allowances[from][msg.sender];
+        if (fromAllowance != type(uint256).max) {
+            allowances[from][msg.sender] = fromAllowance - value;
+        }
+        _transfer(from, to, value);
+        return true;
+    }
+}
